ada6e7702b6ada708002629e921424caf740892f
Tapha Ngum <tapha@live.co.uk>
2010-05-08 19:57:38 +0300

    New layout

--- application/views/home.php
+++ application/views/home.php
@@ -26,74 +26,45 @@
 	
 	<meta name="description" content="Do word of mouth advertising on Twitter" />
 	
-	<meta name="keywords" content="make tweets, make money" />
+	<meta name="keywords" content="make tweets, make money, advertise on twitter, ads twitter, ads" />
 	
 	<meta name="Chief Artist" content="Tapha Ngum" />
 	
 	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />	
 	
 	
-</head>
-<div id="container">
-		<div id="header">
-			<div id="logo">
-				<a href="/">TwitSpare<!--<img src="/twitspare/images/logo.png">--></a>
-			</div>
-			<div id="nav-container">
-			
-									<div id="nav-unauth">
-	<div style="float: left; padding: 14px 9px 9px; font-size: 15px;"><a href="/session/new" style="text-decoration: none; color: rgb(0, 0, 0); font-size: 15px;"><b>Click here to login or create an account</b></a></div>
-	<div style="float: right; width: 155px; padding: 9px;"><a href="/twitspare/index.php/oauth/"><img src="/twitspare/images/sign-in-with-twitter.png"></a></div>
+</head>		
+<body>
+<div id="header_container">
+<div id="header">
+
+	<div id="logo">
+	<a href="/"><img src="/twitspare/images/logo.png"></a>
+	</div>
+	</div>
+	<div id="nav_login_area">
+	<div id="login_area">
+	<div style="float: left; padding: 14px 9px 9px; font-size: 15px;">
+	<a href="/" style="text-decoration: none; color: rgb(0, 0, 0); font-size: 15px;">
+	<b>Click here to create an account or sign-in</b></a></div>
+	<div style="float: right; width: 155px; padding: 9px;"><a id="twitter" href="/"><img src="/twitspare/images/Sign-in-with-Twitter.png"></a></div>
 	<div style="clear: both;"></div>
-</div>							
+</div>
+</div>
+	
+	</div>	
+	</div>
+	
+	<div id="body">
+			
+				<h2></h2>
+			
 			</div>
-			<div style="clear: both;"></div>
-		</div>
-		
-		<div id="arrow-spacer"></div>
-		
-		<div id="content">
-			<div id="content-standard-pad">
-<div style="float: left; width: 560px;">
-<b>Recently Earned</b>
-</div>
 
-<div style="float: right; width: 340px;">
-<div style="text-align: left; font-size: 18px; margin-top: 20px; font-weight: bold;">What is TwitSpare?</div>
-<div style="padding-top: 5px; line-height: 20px text-align: left; font-size: 14px;">TwitSpare let's you do word of mouth advertising<br>on <a target="_blank" href="http://twitter.com" style="font-size: 14px;" class="nav-link">Twitter</a></div>
-
-<div style="padding-top: 20px; text-align: left; font-size: 18px; font-weight: bold;">How do I use TwitSpare?</div>
-<div style="padding-top: 5px; line-height: 20px; text-align: left; font-size: 14px;">You can post pictures to TwitPic from your phone, our <a href="/api.do" style="font-size: 14px;" class="nav-link">API</a>, or through the site itself.</div>
-
-<div style="padding-top: 15px; line-height: 20px; text-align: left; font-size: 14px;">There are also popular twitter clients that have built-in support for TwitPic.</div>
-
-<div style="text-align: left; font-size: 18px; margin-top: 20px; font-weight: bold;">How do I create an account?</div>
-<div style="padding-top: 5px; text-align: left; font-size: 14px; line-height: 20px;">If you have a Twitter account then you already have a Twitpic account, just login to Twitpic with your Twitter username and password and you are all set.<br><br>If you don't have a Twitter account you can sign up for one for free at <a target="_blank" href="http://twitter.com" style="font-size: 14px;" class="nav-link">Twitter.com</a></div>
-
-</div>
-
-<div style="clear: both;"></div>
-
-<div style="padding-top: 15px; padding-bottom: 7px; padding-left: 5px; font-size: 16px;"><b>Recent Ads</b></div>
-
-<div>
-<center>
-Display recents ads here.
-</center></div>
-<div>
-<center>
-</center></div>
-</div>		</div>
 		
 		<div id="footer">
-			<div id="footer-tag">
-			&copy;2010 Dummy Limited, All Rights Reserved
-			</div>
-			<div id="footer-links">
-				<a href="/" class="footer-link">Home</a> &nbsp; <a href="/faq.do" class="footer-link">Faq</a> &nbsp; <a href="/contact.do" class="footer-link">Contact</a>  &nbsp; <a href="/api.do" class="footer-link">API</a> &nbsp; <a href="/terms.do" class="footer-link">Terms</a>
-			</div>
-			<div style="clear: both; height: 25px;">&nbsp;</div>
-		</div>
 		
 	</div>
+	
+</body>	
 </html>
